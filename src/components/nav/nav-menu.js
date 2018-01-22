590de2ba5931f0550e4731f8d505ac7265cc5763
Nate <mlgmainstream@gmail.com>
2018-01-21 20:32:34 -0500

    func click styles added to nav

--- src/components/nav/nav-menu.js
+++ src/components/nav/nav-menu.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default class NavMenu extends React.Component{
 
-  actionClick(evt) {
+  caretActionClick(evt) {
     let menu = document.querySelector('.nav-container');
     menu.classList.toggle('expand');
+  }
+
+  navActionClick(evt) {
+    let itemsSelected = document.querySelector('.nav-list .active');
+    if(itemsSelected){
+      itemsSelected.classList.remove('active');
+    }
+    evt.currentTarget.classList.add('active');
   }
 
   render(){
@@ -13,13 +21,13 @@ export default class NavMenu extends React.Component{
         <div className="nav-container">
           <div className="nav-list">
             <ul>
-              <li className="nav-link"><span className="nav-icon icon-home"></span> HOME</li>
-              <li className="nav-link"><span className="nav-icon icon-dashboard"></span> DASHBOARD</li>
-              <li className="nav-link"><span className="nav-icon icon-services"></span> SERVICES</li>
+              <li className="nav-link" onClick={this.navActionClick.bind(this)}><span className="nav-icon icon-home"></span> HOME</li>
+              <li className="nav-link" onClick={this.navActionClick.bind(this)}><span className="nav-icon icon-dashboard"></span> DASHBOARD</li>
+              <li className="nav-link" onClick={this.navActionClick.bind(this)}><span className="nav-icon icon-services"></span> SERVICES</li>
             </ul>
           </div>
           <div className="nav-action">
-            <div className="action-arrow" onClick={this.actionClick}></div>
+            <div className="action-arrow" onClick={this.caretActionClick}></div>
           </div>
         </div>
       );
